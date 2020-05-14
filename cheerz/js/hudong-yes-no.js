1219b184a82fbc03d4d89fd8fa1689d77b7daab8
瞿毅 <85741950@qq.com>
2020-05-14 18:02:57 +0800

    添加了 《yes-no》游戏逻辑判断+序列帧动画。由于正确错误图片错位，后续需改下图片尺寸。 另外还有5秒限时逻辑未完成

---
+++ cheerz/js/hudong-yes-no.js
@@ -0,0 +1,80 @@
+// 互动游戏 - 图片轮播
+var anw_rw; // 选对选错
+var mv_gj;
+var mv_nt;
+
+function startgame() {
+	var gamepara = localStorage.getItem("gpara");
+	if (gamepara != null) {
+		var paras = gamepara.split("|");
+
+		var cover = paras[0];
+		var sentence = paras[1];
+		anw_rw = parseInt(paras[2]);
+
+		// 背景
+		$("#hd-yn-tuka").attr("src", cover);
+
+		// 句子
+		$("#hd-yn-que").html(sentence);
+
+		// 答案
+		if (anw_rw == 1) {
+			$("#hd-yes").removeClass();
+			$("#hd-no").removeClass();
+			$("#hd-yes").addClass("dui");
+			$("#hd-no").addClass("cuo");
+		} else if (anw_rw == 0) {
+			$("#hd-yes").removeClass();
+			$("#hd-no").removeClass();
+			$("#hd-no").addClass("dui");
+			$("#hd-yes").addClass("cuo");
+		}
+		// 加载动画
+		$("#result").hide();
+		$("#res_gj").hide();
+		$("#res_nt").hide();
+		mv_gj = new seqframe({
+			container: document.getElementById('res_gj'),
+			urlRoot: 'movie/goodjob/',
+			imgType: 'png',
+			frameNumber: 5,
+			framePerSecond: 200,
+			loadedAutoPlay: false,
+			loop: 1,
+		});
+		mv_gj.load();
+
+		mv_nt = new seqframe({
+			container: document.getElementById('res_nt'),
+			urlRoot: 'movie/nicetry/',
+			imgType: 'png',
+			frameNumber: 5,
+			framePerSecond: 200,
+			loadedAutoPlay: false,
+			loop: 1,
+		});
+		mv_nt.load();
+	}
+}
+
+function pro_result(click_rw) {
+	console.log("click:" + click_rw + ", anw:" + anw_rw);
+	if (click_rw == anw_rw) {
+		$("#result").show();
+		$("#res_gj").show();
+		if (mv_gj != null) {
+			mv_gj.play();
+		}
+	} else {
+		$("#result").show();
+		$("#res_nt").show();
+		if (mv_nt != null) {
+			mv_nt.play();
+		}
+	}
+}
+
+function endgame() {
+
+}
