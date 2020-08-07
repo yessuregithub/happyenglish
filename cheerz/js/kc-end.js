7d079305df02b812058d6c761193da6df17d33ee
瞿毅 <85741950@qq.com>
2020-08-07 18:11:04 +0800

    分享保存至相册完成

--- cheerz/js/kc-end.js
+++ cheerz/js/kc-end.js
@@ -41,13 +41,18 @@ function initdata(lid) {
 
 				// 分享设置图
 				var cover = localStorage.getItem('cover');
-				console.log(cover);
+				cover = cover + "?" + new Date().getTime();
+				// cover = "images/donghua.png"; // test
+				// console.log(cover);
+				// console.log(data.avata);
 				$("#fx_nickname").text(data.nickname);
 				$("#fx_coincount").text(data.coincount);
 				$("#fx_wordcount").text(data.wordcount);
 				$("#fx_lessoncount").text(data.lessoncount);
 				$("#cover").attr('src', cover);
-				$("#ustx").html("<img class='acc_imgin' src=" + data.avata + ">");
+				if (data.avata != "") {
+					$("#ustx").html("<img class='acc_imgin' src=" + data.avata + ">");
+				}
 			}
 		},
 		error: function(xhr, type, errorThrown) {
@@ -56,167 +61,192 @@ function initdata(lid) {
 	});
 }
 
-shareY(T) {
-	$(".mui-ex").hide();
-	plus.nativeUI.showWaiting("正在生成图片...");
-	if (T == 1) {
-		var cntElem = document.getElementById('fen');
-	} else {
-		var cntElem = document.getElementById('fen');
-	}
+function share_div(share_type) {
+	console.log('share_div:' + share_type);
+	clipDomImage('fen', 'hpyy_share.png', false, true, share_type);
+}
 
-	var shareContent = cntElem; //需要截图的包裹的（原生的）DOM 对象
-	var width = shareContent.offsetWidth; //获取dom 宽度
-	var height = shareContent.offsetHeight; //获取dom 高度
-	var canvas = document.createElement("canvas"); //创建一个canvas节点
-	var scale = 2; //定义任意放大倍数 支持小数
-	canvas.width = width * scale; //定义canvas 宽度 * 缩放
-	canvas.height = height * scale; //定义canvas高度 *缩放
-	canvas.getContext("2d").scale(scale, scale); //获取context,设置scale 
-	var opts = {
-		scale: scale, // 添加的scale 参数
-		canvas: canvas, //自定义 canvas
-		// logging: true, //日志开关，便于查看html2canvas的内部执行流程
-		width: width, //dom 原始宽度
-		height: height,
-		useCORS: true // 【重要】开启跨域配置
+// 截图
+function clipDomImage(domID, fileName, showScreen, save_doc, share_type) {
+	console.log("clipDomImage");
+	var odiv = document.getElementById(domID);
+	if (!odiv) return;
+	
+	plus.nativeUI.showWaiting();
+	
+	var left = odiv.getBoundingClientRect().left;
+	var top = odiv.getBoundingClientRect().top;
+	var width = odiv.getBoundingClientRect().width;
+	var height = odiv.getBoundingClientRect().height;
+
+	var clipregin = {
+		top: top + 'px',
+		left: left + 'px',
+		width: width + 'px',
+		height: height + 'px'
 	};
 
-	html2canvas(shareContent, opts).then(function(canvas) {
-		// H5 plus事件处理
+	console.log('clipregin:' + JSON.stringify(clipregin));
 
-		var context = canvas.getContext('2d');
-		// 【重要】关闭抗锯齿
-		context.mozImageSmoothingEnabled = false;
-		context.webkitImageSmoothingEnabled = false;
-		context.msImageSmoothingEnabled = false;
-		context.imageSmoothingEnabled = false;
-		var dataUrl = canvas.toDataURL("image/jpeg");
+	var bitmap = new plus.nativeObj.Bitmap('bitmap');
 
-		var bitmap = new plus.nativeObj.Bitmap();
-		var result = new Date().getTime();
-		var filename = result + "_picture" + ".jpeg";
-		bitmap.loadBase64Data(dataUrl);
+	var ws = plus.webview.currentWebview();
+	ws.draw(bitmap, function() {
+		console.log('截屏绘制图片成功');
+	}, function(e) {
+		console.log('截屏绘制图片失败：' + JSON.stringify(e));
+	}, {
+		clip: clipregin
+	});
 
-		bitmap.save(
-			"_doc/" + filename, {
-				overwrite: true
-			},
-			function(i) {
-				//保存到系统相册
-				plus.gallery.save(
-					i.target,
-					function(d) {
-						//销毁Bitmap图片
-						bitmap.clear();
-						console.log("保存图片到相册成功");
-					},
-					function() {
-						console.log("保存保存失败");
+	// 显示到屏幕上
+	if (showScreen) {
+		var img = new Image();
+		img.src = bitmap.toBase64Data();
+		if (document.getElementById('show_box')) {
+			document.getElementById('show_box').appendChild(img);
+		}
+	}
+
+	// 保存相册
+	if (save_doc) {
+		save_share(bitmap, fileName, share_type);
+	} else {
+		bitmap.clear();
+	}
+	
+	plus.nativeUI.closeWaiting();
+}
+
+
+function save_share(bitmap, filename, share_type) {
+	var savealbum = false;
+	bitmap.save(
+		"_doc/" + filename, {
+			overwrite: true
+		},
+		function(i) {
+			//保存到系统相册
+			plus.gallery.save(
+				i.target,
+				function(d) {
+					//销毁Bitmap图片
+					bitmap.clear();
+					savealbum = true;
+					console.log("保存图片到相册成功");
+					
+					// 分享
+					if (share_type != -1) {
+						var path = "file://" + plus.io.convertLocalFileSystemURL("_doc/" + filename);
+						shareImage(path, share_type); //传入参数调用微信分享接口			
+					} else {
+						plus.nativeUI.alert('已保存至您的相册');
 					}
-				);
-			},
-			function() {
-				bitmap.clear();
-			}
-		);
-		plus.nativeUI.closeWaiting();
-		var path = "file://" + plus.io.convertLocalFileSystemURL("_doc/" + filename);
-		shareImage(path); //传入参数调用微信分享接口
-	})
-	var shares = null;
-	var sweixin = null;
-	var buttons = [{
-			title: '我的好友',
-			extra: {
-				scene: 'WXSceneSession'
-			}
+				},
+				function() {
+					savealbum = false;
+					console.log("保存保存失败");
+				}
+			);
 		},
-		{
-			title: '朋友圈',
-			extra: {
-				scene: 'WXSceneTimeline'
-			}
-		},
-		{
-			title: '我的收藏',
-			extra: {
-				scene: 'WXSceneFavorite'
-			}
+		function() {
+			bitmap.clear();
 		}
-	];
+	);
+}
 
-	// H5 plus事件处理
-	// function plusReady() {
-	// 	updateSerivces();
-	// }
-	// if (window.plus) {
-	// 	plusReady();
-	// } else {
-	// 	document.addEventListener('plusready', plusReady, false);
-	// }
-	/**
-	 * 更新分享服务
-	 */
-
-	function updateSerivces() {
-		plus.share.getServices(function(s) {
-			shares = {};
-			for (var i in s) {
-				var t = s[i];
-				shares[t.id] = t;
-			}
-			sweixin = shares['weixin'];
-		}, function(e) {
-			console('获取分享服务列表失败：' + e.message);
-		});
-	}
-	// 分享图片
-	function shareImage(path) {
-		var msg = {
-			type: 'image'
-		};
-
-		msg.pictures = [path];
-		//console.log(msg.pictures)
-		sweixin ? plus.nativeUI.actionSheet({
-			title: '分享图片到微信',
-			cancel: '取消',
-			buttons: buttons
-		}, function(e) {
-			(e.index > 0) && share(sweixin, msg, buttons[e.index - 1]);
-		}) : plus.nativeUI.alert('当前环境不支持微信分享操作!');
-	}
-
-	// 分享
-	function share(srv, msg, button) {
-
-		if (!srv) {
-			//console('无效的分享服务！');
-			return;
+// 分享分类
+var shares = null;
+var sweixin = null;
+var buttons = [{
+		title: '我的好友',
+		extra: {
+			scene: 'WXSceneSession'
 		}
-		button && (msg.extra = button.extra);
-		// 发送分享
-		if (srv.authenticated) {
-			//console('---已授权---');
-			doShare(srv, msg);
-		} else {
-			//console('---未授权---');
-			srv.authorize(function() {
-				doShare(srv, msg);
-			}, function(e) {
-				//console('认证授权失败：' + JSON.stringify(e));
-			});
+	},
+	{
+		title: '朋友圈',
+		extra: {
+			scene: 'WXSceneTimeline'
 		}
 	}
+];
+
+/**
+ * 分享服务
+ */
+function updateSerivces() {
+	plus.share.getServices(function(s) {
+		shares = {};
+		for (var i in s) {
+			var t = s[i];
+			shares[t.id] = t;
+		}
+		sweixin = shares['weixin'];
+	}, function(e) {
+		console('获取分享服务列表失败：' + e.message);
+	});
+}
+
+// 分享图片
+function shareImage(path, sharetype) {
+	var msg = {
+		type: 'image'
+	};
+
+	msg.pictures = [path];
+	if (sharetype == -1) {
+		// 相册
+		share(sweixin, msg, -1);
+		return;
+	}
+
+	if (sweixin) {
+		// 好友
+		if (sharetype == 1) {
+			share(sweixin, msg, buttons[0]);
+		} else if (sharetype == 2) {
+			// 朋友圈
+			share(sweixin, msg, buttons[1]);
+		}
+	} else {
+		plus.nativeUI.alert('当前环境不支持微信分享操作!');
+	}
+}
+
+// 分享
+function share(srv, msg, button) {
+
+	if (!srv) {
+		//console('无效的分享服务！');
+		return;
+	}
+	if (button.extra == -1) {
+		if (savealbum) {
+			plus.nativeUI.alert('保存相册成功');
+		}
+		return;
+	}
+	button && (msg.extra = button.extra);
 	// 发送分享
-	function doShare(srv, msg) {
-		//console(JSON.stringify(msg));
-		srv.send(msg, function() {
-			console('分享到"' + srv.description + '"成功！');
+	if (srv.authenticated) {
+		//console('---已授权---');
+		doShare(srv, msg);
+	} else {
+		//console('---未授权---');
+		srv.authorize(function() {
+			doShare(srv, msg);
 		}, function(e) {
-			console('分享到"' + srv.description + '"失败: ' + JSON.stringify(e));
+			//console('认证授权失败：' + JSON.stringify(e));
 		});
 	}
-
-},
+}
+// 发送分享
+function doShare(srv, msg) {
+	//console(JSON.stringify(msg));
+	srv.send(msg, function() {
+		console('分享到"' + srv.description + '"成功！');
+	}, function(e) {
+		console('分享到"' + srv.description + '"失败: ' + JSON.stringify(e));
+	});
+}
